bf26b1d457db1859ed34c399a3ab64d40b488dc8
aayobam <aayobam@gmail.com>
2020-05-09 10:19:38 +0100

    full update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 let creditScoreDeterminant = 650;
 let loanApplicationList = {};
-let creditScore;
+
 
 class loanApplication {
     applicantname;
@@ -10,19 +10,17 @@ class loanApplication {
     creditScore;
     loanpurpose;
     loanamount;
-    Id;
 };
 
 
 //create an instance of the class and initialise values to each of them
 function initializeLoan(){
     let la1 = new loanApplication();
-    la1.Id = createRandomCode(10)
     la1.applicantname = "Mr John Travolta";
     la1.dateofbirth = new Date(1980, 11, 30);
     la1.incomefield = 60000;
-    la1.creditScore = 600;
-    la1.loanpurpose = "my partner need the loan to purchase new equipment"
+    la1.creditScore = 500;
+    la1.loanpurpose = "my partner need the loan to purchase new equipment";
     la1.loanamount = 8000;
     loanApplicationList[0] = la1;
 
@@ -32,27 +30,27 @@ function initializeLoan(){
     la2.dateofbirth = new Date(1960, 8, 22);
     la2.incomefield = 135000;
     la2.creditScore = 670;
-    la2.loanpurpose = "my partner need the loan to purchase new equipment"
+    la2.loanpurpose = "i need this to go on holiday vacation and honeymoon";
     la2.loanamount = 50000;
-    la2.Id = createRandomCode(10)
+    
     loanApplicationList[1] = la2;
 
 
     let la3 = new loanApplication();
     la3.applicantname = "Mrs Debbie Row";
-    la3.dateofbirth = new Date(19720, 5, 18);
+    la3.dateofbirth = new Date(1972, 5, 18);
     la3.incomefield = 115000;
-    la3.creditScore = 450;
-    la3.loanpurpose = "my partner need the loan to purchase new equipment"
+    la3.creditScore = 499;
+    la3.loanpurpose = "i and my family need to secure a new house";
     la3.loanamount = 30000;
-    la3.Id = createRandomCode(10)
+    
     loanApplicationList[2] = la3;
-
 };
 initializeLoan();
 
 
-//creates a reference code should in case there are complaints from applicants
+/*
+creates a reference code should in case there are complaints from applicants
 function createRandomCode(length) {
     let randomCodes = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -61,66 +59,89 @@ function createRandomCode(length) {
         randomCodes += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
  };
+ createRandomCode(length);
+ */
 
  //bind each applicants to drop down.
 function bindLoansToDropDown(){
+    let dropDown = document.getElementById('loanApplications');
+
     for (let objects in loanApplicationList){
         let eachApplicants = loanApplicationList[objects];
-
-        let dropDown = document.getElementById('loanApplications');
         let opt = document.createElement('option');
+        opt.setAttribute("data-index", objects);
         opt.appendChild(document.createTextNode("Application of " + eachApplicants.applicantname));
         opt.value = eachApplicants.applicantname.toString();
-        dropDown.appendChild(opt);
-
-        dropDown = document.addEventListener('')
+        dropDown.appendChild(opt); 
+    }
+    dropDown.onchange = function(e) {
+        var index = this.options[this.selectedIndex].getAttribute("data-index");
+        var obj = loanApplicationList[index];
+        document.getElementById('fullName').value = obj.applicantname;
+        document.getElementById('dateOfBirth').value = obj.dateofbirth;
+        document.getElementById('incomeField').value = parseFloat(obj.incomefield);
+        document.getElementById('credit-score').value = obj.creditScore;
+        document.getElementById('loanPurpose').value = obj.loanpurpose;
+        document.getElementById('loanAmount').value = parseFloat(obj.loanamount);
+        function loanStatusAlert(){
+            let message1 = `Dear ${obj.applicantname}, your loan application of ${'$' + obj.loanamount} has been approved.`;
+            let message2 = `Dear ${obj.applicantname}, your loan application of ${'$' + obj.loanamount} 
+            was not approved because of too low credit score.`;
+            if (creditScore <= creditScoreDeterminant && creditScore >= 500){
+                document.getElementById('loanStatus').innerText = message1;
+            }
+            else{
+                document.getElementById('loanStatus').innerText = message2;
+            };
+        };
+        loanStatusAlert();
     }
 };
 bindLoansToDropDown();
 
 
-//this check for the elibility of applicants for loan using credit score
-function loanEligibilityIneligibilityMessage(){
-    if (creditScore <= creditScoreDeterminant && creditScore > 500){
-        let status = document.getElementById('loanStatus')
-        let alert = `Dear ${eachApplicants.applicantname}, your loan application of ${"$" + eachApplicants.loanamount}
-        has been granted and your reference code is ${eachApplicants.Id}`;
-        let message = document.createTextNode(alert);
-        status.appendChild(message)
-    }
-    else if(creditScore < 500){
-        let status = document.getElementById('loanStatus')
-        let alert = `Dear ${eachApplicants.applicantname}, your loan application of ${"$" + eachApplicants.loanamount}
-        has been denied and your reference code is ${eachApplicants.Id}`;
-        let message = document.createTextNode(alert);
-        status.appendChild(message);
-    }
-    else{
-        let status = document.getElementById('loanStatus')
-        let alert = `Loan application not found`;
-        let message = document.createTextNode(alert);
-        status.appendChild(message);
-    };
-}
-loanEligibilityIneligibilityMessage();
-
-
 //This checks for the datatype of data on the forms.
 function formsInputType(){
-    if (isNaN(document.getElementById('dateOfBirth').innerText)) {
-        document.getElementById('dateOfBirth').innerText = 'undefined';
+    if (isNaN(document.getElementById('dateOfBirth').value)) {
+        document.getElementById('dateOfBirth').value = 'undefined';
     }
 
-    else if(isNaN(document.getElementById('incomeField').innerText)) {
-        document.getElementById('incomeField').innerText = 'undefined';
+    else if(isNaN(document.getElementById('incomeField').value)) {
+        document.getElementById('incomeField').value = 'undefined';
     }
 
-    else if(isNaN(document.getElementById('credit-score').innerText)) {
-        document.getElementById('credit-score').innerText = 'undefined';
+    else if(isNaN(document.getElementById('credit-score').value)) {
+        document.getElementById('credit-score').value = 'undefined';
     }
 
-    else if(isNaN(document.getElementById('loanAmount').innerText)) {
-        document.getElementById('loanAmount').innerText = 'undefined';
+    else if(isNaN(document.getElementById('loanAmount').value)) {
+        document.getElementById('loanAmount').value = 'undefined';
     };
 }
 formsInputType();
+
+
+const buttonApply = document.getElementById("btn-apply").addEventListener('click', function() {
+
+    if (document.getElementById("fullName").value = " "){
+        document.getElementById('for-fullname').classList.remove('display');
+    }
+    else if (document.getElementById("dateOfBirth").value = " "){
+        document.getElementById('for-dateOfBirth').classList.remove('display');
+    }
+    else if (document.getElementById("incomeField").value = " "){
+        document.getElementById('for-incomeField').classList.remove('display');
+    }
+    else if (document.getElementById("credit-score").value = " "){
+        document.getElementById('for-creditscore').classList.remove('display');
+    }
+    else if (document.getElementById("loanPurpose").value = " "){
+        document.getElementById('for-loanPurpose').classList.remove('display');
+    }
+    else if (document.getElementById("loanAmount").value = " "){
+        document.getElementById('for-loanAmount').classList.remove('display');
+    };
+
+});git
+
+
